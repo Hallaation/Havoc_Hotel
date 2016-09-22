cf8e3df2a5d5d72d4f52f179201ac2565a63da02
Louis Nguyen <louis.nguyen@ad.aie.edu>
2016-09-22 17:19:15 +1000

    1 Sided platform movement. Trigger on prefabs

--- Havoc Hotel/Assets/HavocHotel/Scripts/MovementTest.cs
+++ Havoc Hotel/Assets/HavocHotel/Scripts/MovementTest.cs
@@ -3,87 +3,79 @@ using System.Collections;
 
 public class MovementTest : MonoBehaviour
 {
-	const float _SPEED = 10.0f;
-	const int _ROTATION_SPEED = 20;
-	const float _JUMP_SPEED = 35.0f;
-	const float _GRAVITY = 100.0f;
-	public Transform lookAt;
-	bool wentThrough = false;
-	bool disable = true;
-	float timer = 0.0f;
-	private Vector3 movementDirection = Vector3.zero;
+    const float _SPEED = 10.0f;
+    const int _ROTATION_SPEED = 20;
+    const float _JUMP_SPEED = 20.0f;
+    const float _GRAVITY = 100.0f;
+    public Transform lookAt;
+    bool wentThrough = false;
+    bool disable = true;
+    float timer = 0.0f;
+    private Vector3 movementDirection = Vector3.zero;
 
-	public GameObject platformController;
+    public int PlayerNumber;
+    public GameObject platformController;
 
-	//update every frame
-	void Update()
-	{
-		CharacterController temp = GetComponent<CharacterController>();
-		if (temp.isGrounded || disable)
-		{
-			this.transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * _SPEED , 0);
-			if (temp.isGrounded)
-			{
-				if (Input.GetButton("Jump"))
-				{
-					transform.position += new Vector3(0 , _JUMP_SPEED * Time.deltaTime);
-					movementDirection.y = _JUMP_SPEED;
-					//Debug.Log("Jumping");
-				}
-			}
-		}
-		timer += Time.deltaTime;
+    //update every frame
+    void Update()
+    {
+        CharacterController temp = GetComponent<CharacterController>();
+        if (temp.isGrounded || disable)
+        {
+            this.transform.position -= new Vector3(Input.GetAxis(PlayerNumber +"_Horizontal") * Time.deltaTime * _SPEED , 0);
+            if (temp.isGrounded)
+            {
+                if (Input.GetButton(PlayerNumber + "_Fire") && Input.GetAxis(PlayerNumber + "_Vertical") >= 0)
+                {
+                    transform.position += new Vector3(0 , _JUMP_SPEED * Time.deltaTime);
+                    movementDirection.y = _JUMP_SPEED;
+                    //Debug.Log("Jumping");
+                }
+            }
+        }
+        timer += Time.deltaTime;
+        
+        movementDirection.y -= _GRAVITY * Time.deltaTime;
+        temp.Move(movementDirection * Time.deltaTime);
 
-		movementDirection.y -= _GRAVITY * Time.deltaTime;
-		temp.Move(movementDirection * Time.deltaTime);
+        //if (wentThrough)
+        //{
+        //    foreach (Collider i in platformController.GetComponentsInChildren<Collider>())
+        //    {
+        //        Physics.IgnoreCollision(temp , i , false);
+        //        Debug.Log(i.name);
+        //    }
+        //}
 
-		//if (wentThrough)
-		//{
-		//    foreach (Collider i in platformController.GetComponentsInChildren<Collider>())
-		//    {
-		//        Physics.IgnoreCollision(temp , i , false);
-		//        Debug.Log(i.name);
-		//    }
-		//}
-	}
+        if (Input.GetButton(PlayerNumber + "_Fire") && Input.GetAxis(PlayerNumber + "_Vertical") < 0)
+        {
+            JumpDown();
+        }
+    }
 
-	//this is where the collision is ignored. once the player hits a platform with the name "platform" in it the collisions for the player and this collider are ignored. which are re enabled later after the trigger exit
-	void OnControllerColliderHit(ControllerColliderHit hit)
-	{
-		CharacterController temp = GetComponent<CharacterController>();
-		if (!temp.isGrounded)
-		{
-			//Debug.Log("Hit something");
-			if (hit.collider.name.Contains("Platform"))
-			{
-				if (!wentThrough)
-				{
-					Physics.IgnoreCollision(temp , hit.collider);
-					Debug.Log(hit.collider.name);
-					Debug.Log(GetComponent<Collider>().name);
-				}
-			}
-		}
+    void OnTriggerEnter(Collider other)
+    {
+        CharacterController temp = GetComponent<CharacterController>();
 
-		if (Input.GetKey(KeyCode.K))
-		{
-			Debug.Log(wentThrough);
-		}
-	}
+        Debug.Log("TRIGGERED");
+        Physics.IgnoreCollision(temp , other.GetComponentInParent<Collider>());
+        Debug.Log(other.name);
+        Debug.Log(GetComponent<Collider>().name);
 
-	void OnTriggerEnter(Collider other)
-	{
-		Debug.Log("TRIGGERED");
-	}
+    }
+    //once exiting the trigger, the parent's collider will no longer ignore collisions
+    void OnTriggerExit(Collider other)
+    {
 
-	//once exiting the trigger, the parent's collider will no longer ignore collisions
-	void OnTriggerExit(Collider other)
-	{
+        CharacterController temp = GetComponent<CharacterController>();
+        wentThrough = true;
+        Physics.IgnoreCollision(temp , other.GetComponentInParent<Collider>() , false);
+        //Debug.Log(other.transform.parent.GetComponent<Collider>().name);
 
-		CharacterController temp = GetComponent<CharacterController>();
-		wentThrough = true;
-		Physics.IgnoreCollision(temp , other.transform.parent.GetComponent<Collider>() , false);
-		//Debug.Log(other.transform.parent.GetComponent<Collider>().name);
+    }
 
-	}
+    void JumpDown()
+    {
+        Debug.Log("Jumped down");
+    }
 }
