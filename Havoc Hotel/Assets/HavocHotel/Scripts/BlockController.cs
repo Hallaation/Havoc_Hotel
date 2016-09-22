ceae48857ba959b75e45a31c28b4970f9a466548
Krishna Shah <krishna.shah@ad.aie.edu>
2016-09-22 17:24:04 +1000

    Created level generation scene Created script for level movement speed

---
+++ Havoc Hotel/Assets/HavocHotel/Scripts/BlockController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour
+{
+    public float m_fOverworldSpeed;
+
+    //  public static Object prefab = Resources.Load("HavocHotel/Prefabs/Levels/Level_001");
+    //  public static BlockController Create()
+    //  {
+    //      GameObject newObject = Instantiate(prefab) as GameObject;
+    //      BlockController yourObject = newObject.GetComponent<BlockController>();
+    //      //do additional initialization steps here
+    //      return yourObject;
+    //  }
+    //
+
+
+    // List<GameObject> prefabList = new List<GameObject>();
+    // public GameObject Prefab1;
+    // public GameObject Prefab2;
+    // public GameObject Prefab3;
+
+    public GameObject prefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_fOverworldSpeed = 0.0f;
+        // prefabList.Add(Prefab1);
+        // prefabList.Add(Prefab2);
+        // prefabList.Add(Prefab3);
+        //
+        // int prefabIndex = UnityEngine.Random.Range(0, 3);
+        // Instantiate(prefabList[prefabIndex]);
+
+
+        for (int i = 0; i < 10; i++)
+            Instantiate(prefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        m_fOverworldSpeed += Time.deltaTime;
+    }
+}
+
+// Update is called once per frame
+
+
---
+++ Havoc Hotel/Assets/HavocHotel/Scripts/MoveLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLevel : MonoBehaviour
+{
+    public float m_fLevelSpeed = 1.0f; //-modifiable value for designers (modifiable in unity)- initial speed of level
+    public float m_fSpeedIncrease = 1.1f; //-modifiable value to change rate at which level increases in speed
+
+    public BlockController refController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.down * refController.m_fOverworldSpeed * Time.deltaTime / 25);
+    }
+}
