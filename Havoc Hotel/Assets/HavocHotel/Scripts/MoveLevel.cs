ffa8a1072286e1f1c634bd99dc778a44bb9cbe90
Krishna Shah <krishna.shah@ad.aie.edu>
2016-10-06 11:38:39 +1100

    Updating level spawn and movement. ** Level requires rigibody for deleting

--- Havoc Hotel/Assets/HavocHotel/Scripts/MoveLevel.cs
+++ Havoc Hotel/Assets/HavocHotel/Scripts/MoveLevel.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MoveLevel : MonoBehaviour
 {
-    public float m_fLevelSpeed = 1.0f; //-modifiable value for designers (modifiable in unity)- initial speed of level
+    public float m_fLevelSpeed = 10.0f; //-modifiable value for designers (modifiable in unity)- initial speed of level
     public float m_fSpeedIncrease = 1.1f; //-modifiable value to change rate at which level increases in speed
 
     public BlockController refController;
@@ -17,11 +17,7 @@ public class MoveLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = new Vector3(Mathf.Clamp(Time.time, 0.0F, 3.0F), 0, 0);
-
-      //  m_fLevelSpeed += Time.deltaTime;
-
-        transform.Translate(Vector3.down * refController.m_fOverworldSpeed * Time.deltaTime);
+        transform.Translate(Vector3.down * 10 * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider other)
@@ -32,6 +28,8 @@ public class MoveLevel : MonoBehaviour
             Destroy(this.gameObject);
             //refController.Spawn();
         }
+
+        Debug.Log("Destroy this");
     }
 
    
