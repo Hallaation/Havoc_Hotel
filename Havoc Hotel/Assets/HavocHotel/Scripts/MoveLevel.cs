a43ac04dcac9474161b1c2c49503545aaa45ce1b
Krishna Shah <krishna.shah@ad.aie.edu>
2016-10-07 17:13:56 +1100

    added count down to level start. added possible stage-speed-power-ups. commented a lot of me code

--- Havoc Hotel/Assets/HavocHotel/Scripts/MoveLevel.cs
+++ Havoc Hotel/Assets/HavocHotel/Scripts/MoveLevel.cs
@@ -6,7 +6,7 @@ public class MoveLevel : MonoBehaviour
     public float m_fLevelSpeed = 1.0f; //-modifiable value for designers (modifiable in unity)- initial speed of level
     
 
-    public BlockController refController;
+    public BlockController refController; // refrance BlockController
 
 
     // Use this for initialization
@@ -17,18 +17,18 @@ public class MoveLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (refController.m_bRunning)
+        if (refController.m_bRunning) 
         {
-            transform.Translate(Vector3.down * m_fLevelSpeed * Time.deltaTime);
-            m_fLevelSpeed = refController.m_fOverworldSpeed;
+            transform.Translate(Vector3.down * m_fLevelSpeed * Time.deltaTime); // move level
+            m_fLevelSpeed = refController.m_fOverworldSpeed; // makes levelSpeed OBEY worldSpeed
         }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)// when colliding with box collider at bottom  
     {
-        if (other.tag == "Finish")
+        if (other.tag == "Finish") // 
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); // destroy level blocks
         }
 
     }
