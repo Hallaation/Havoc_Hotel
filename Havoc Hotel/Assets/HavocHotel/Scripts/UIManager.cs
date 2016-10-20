2a0bd4d65c3478a5464a9d612b9a2dcf5a5843fc
Krishna Shah <krishna.shah@ad.aie.edu>
2016-10-20 17:06:32 +1100

    added menu scene and script

---
+++ Havoc Hotel/Assets/HavocHotel/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class UIManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(0); // loads scene with index of zero (can find index of scene with the build settings)
+
+    }
+      
+}
+
