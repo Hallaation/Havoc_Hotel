a3eb1b4819eac4ae3957d62a150380ec9460bec8
Louis <louis_nguyen07@hotmail.com>
2016-10-27 16:50:04 +1100

    Added cancel button in input manager. game scene changing a referencing in a gameobject. Block controller now checks if the game is paused or not. MenuSelection does nothing right now. Might delete later. UIManager now checks to see if the game has started to let the players to load the start menu

---
+++ Havoc Hotel/Assets/HavocHotel/Scripts/MenuSelection.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSelection : MonoBehaviour
+{
+
+
+    public GameObject[] m_panelArray;
+    //Main Menu - 1
+    //Quit Menu - 2
+    //Credits -  3
+    //Puase panel - 4
+    // Use this for initialization
+
+    GameObject EventSystem;
+    public GameObject currentlySelectedObject;
+
+    void Start()
+    {
+        EventSystem = GameObject.Find("EventSystem");
+        EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //EventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(currentlySelectedObject);
+    }
+}
