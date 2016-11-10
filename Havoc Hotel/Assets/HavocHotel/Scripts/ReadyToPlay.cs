ec98f4eda99ab51a45cabd04aff698feb3271a0d
BuildTools <unconfigured@null.spigotmc.org>
2016-11-10 21:20:37 +1100

    Didnt revert a change while testing things.

--- Havoc Hotel/Assets/HavocHotel/Scripts/ReadyToPlay.cs
+++ Havoc Hotel/Assets/HavocHotel/Scripts/ReadyToPlay.cs
@@ -38,7 +38,7 @@ public class ReadyToPlay : MonoBehaviour
                     {
                         item.GetComponent<Movement>().DontDestroyOnLoad();
                     }
-                    SceneManager.LoadScene(2);
+                    SceneManager.LoadScene(3);
                 }
 
             }
