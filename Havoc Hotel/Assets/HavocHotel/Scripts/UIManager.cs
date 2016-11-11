5024519d45a685f3fb6b8d5cc53e208115f71ac7
Louis <louis_nguyen07@hotmail.com>
2016-11-11 16:02:19 +1100

    Somebody commented out needed code which broke everything to do with pausing the game

--- Havoc Hotel/Assets/HavocHotel/Scripts/UIManager.cs
+++ Havoc Hotel/Assets/HavocHotel/Scripts/UIManager.cs
@@ -65,8 +65,8 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // if (GameStarted && m_PlayersInGameScene)
-      //  {
+        if (GameStarted && m_PlayersInGameScene)
+        {
             if (Input.GetButtonDown("Cancel"))
             //if(Input.GetKeyDown(KeyCode.Escape))
             {
@@ -89,7 +89,7 @@ public class UIManager : MonoBehaviour
                 m_bResumeGame = false;
                 refCountDownTimer.SetActive(false);
             }
-       // }
+       }
 
     }
 
