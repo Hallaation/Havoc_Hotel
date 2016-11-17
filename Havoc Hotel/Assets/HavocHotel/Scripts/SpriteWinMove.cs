b3c841264d481c2fe176b2f61ac0eb84fe8a55f2
Louis <louis_nguyen07@hotmail.com>
2016-11-18 09:57:33 +1100

    Added the script that moves the player win sprite. Scaled up the background to cover up a gap. Timed menu to show up slightly after the winning sprite ends its travel

---
+++ Havoc Hotel/Assets/HavocHotel/Scripts/SpriteWinMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteWinMove : MonoBehaviour
+{
+
+
+    private Vector3 m_vMoveTowards;
+
+    public float m_fSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        m_vMoveTowards = new Vector3(0.194444444f, 0.05f, 0.31f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.localPosition.x <= m_vMoveTowards.x)
+        {
+            Debug.Log("true");
+
+        }
+        else
+        {
+            this.transform.localPosition -= new Vector3(m_fSpeed * Time.deltaTime, 0, 0);
+            Debug.Log("False");
+        }
+    }
+}
+
