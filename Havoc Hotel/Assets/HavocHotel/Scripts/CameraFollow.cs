3b499835f15d6c672b1c911567ba8768fb11a723
Louis Nguyen <nguyenlouis4@gmail.com>
2016-11-25 00:16:25 +1100

    Added a tilt slider to the camera follow script. Also added an up and horizontal slider for their distances.

--- Havoc Hotel/Assets/HavocHotel/Scripts/CameraFollow.cs
+++ Havoc Hotel/Assets/HavocHotel/Scripts/CameraFollow.cs
@@ -1,35 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
     public GameObject m_gObjectToFollow;
-    [Range(2.0f, 50.0f)]
+    [Range(2.0f , 50.0f)]
     public float m_fDistanceSlider;
+
+    [Range(-10.0f , 10.0f)]
+    public float m_fUpDistance;
+
+    [Range(-10.0f , 10.0f)]
+    public float m_fHorizontalDistance;
+
+    [Range(-180 , 180)]
+    public float m_fXTilt;
+
     public bool m_bLookAt;
     private GameObject oldPosition;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         oldPosition = this.gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (m_gObjectToFollow)
         {
             if (m_bLookAt)
             {
                 this.transform.LookAt(m_gObjectToFollow.transform);
-                this.transform.position = new Vector3(oldPosition.transform.position.x, oldPosition.transform.position.y, m_fDistanceSlider);
+                this.transform.position = new Vector3(oldPosition.transform.position.x , oldPosition.transform.position.y , m_fDistanceSlider);
             }
             else
             {
-                this.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
-                this.transform.position = new Vector3(m_gObjectToFollow.transform.position.x, m_gObjectToFollow.transform.position.y, m_gObjectToFollow.transform.position.z + m_fDistanceSlider);
+                this.transform.rotation = Quaternion.Euler(new Vector3(0 , 180 , 0));
+                this.transform.position = new Vector3(m_gObjectToFollow.transform.position.x + m_fHorizontalDistance , m_gObjectToFollow.transform.position.y + m_fUpDistance , m_gObjectToFollow.transform.position.z + m_fDistanceSlider);
+                this.transform.localRotation = Quaternion.Euler(new Vector3(m_fXTilt , 180));
             }
         }
         else
         {
             this.transform.position = oldPosition.transform.position;
         }
-	}
+    }
 }
